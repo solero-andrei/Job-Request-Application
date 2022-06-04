52f5b47ff4d817e7e4dc0ab539025313f1ab31c6
Luna-Jennifer <jennluna1521@gmail.com>
2022-06-03 18:35:42 -0700

    updated

--- src/components/login.component.js
+++ src/components/login.component.js
@@ -3,7 +3,24 @@ import React, {Component} from 'react';
 export default class Login extends Component{
     render(){
         return(
-            <h2>This is the login page</h2>
+            <form>
+                <h3>Login</h3>
+
+                <div className="form-group">
+                    <label>Email</label>
+                    <input type="email" className="form-control" placeholder="Email"
+                        onChange={e => this.email = e.target.value}/>
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Password"
+                        onChange={e => this.password = e.target.value}/>
+                </div>
+
+                <button className= "btn btn-primary btn-block">Login</button>
+            </form>
+            
         )
         
     }
